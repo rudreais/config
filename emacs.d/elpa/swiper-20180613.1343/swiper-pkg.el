dedd4741b23a3e409dcbe337ac0b63969de1c003
rudreais <valentinlattay@gmail.com>
2018-06-14 02:33:30 +0200

    fresh emacs

---
+++ emacs.d/elpa/swiper-20180613.1343/swiper-pkg.el
@@ -0,0 +1,1 @@
+(define-package "swiper" "20180613.1343" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "5658acb417ed0b6bbb1307bfd068e7136adf6fce" :keywords '("matching") :url "https://github.com/abo-abo/swiper")
