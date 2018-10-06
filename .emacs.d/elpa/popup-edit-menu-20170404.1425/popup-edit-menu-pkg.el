43b2ce3104c6bf632587704d2522c806c2ae7a4c
rudreais <valentin.lattay@rudreais.com>
2018-10-06 15:36:24 +0200

    deleting old files

--- .emacs.d/elpa/popup-edit-menu-20170404.1425/popup-edit-menu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "popup-edit-menu" "20170404.1425" "a popup context edit menu package" '((emacs "24")) :commit "925600a6e29183841199e866cf55e566a6a1b002" :keywords '("lisp" "pop-up" "context" "edit" "menu") :authors '(("Debugfan Chin" . "debugfanchin@gmail.com")) :maintainer '("Debugfan Chin" . "debugfanchin@gmail.com"))
--- .emacs.d/elpa/swiper-20180613.1343/swiper-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "swiper" "20180613.1343" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "5658acb417ed0b6bbb1307bfd068e7136adf6fce" :keywords '("matching") :url "https://github.com/abo-abo/swiper")
